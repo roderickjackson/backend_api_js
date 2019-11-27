4bf6b2b846f3e4802a307575edb4217eababc710
Roderick Jackson-Bradley <jacksonb@adobe.com>
2019-11-26 19:28:06 -0600

    update gitignore

--- server/dbKey.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable semi */
-// eslint-disable-next-line strict
-module.exports =
-'mongodb://roderick3427:.eQ13Dq3@cluster0-shard-00-00-wuodb.mongodb.net:27017,cluster0-shard-00-01-wuodb.mongodb.net:27017,cluster0-shard-00-02-wuodb.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
